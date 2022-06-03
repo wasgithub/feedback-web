1e1ab0da308f73be98d152cb14527425bcbeb241
Washington Alexandre da Silva <was.alexandre42@gmail.com>
2022-06-02 22:07:34 -0300

    feature: add component Widget

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+
+import React from "react";
+import { Widget } from "./components/Widget";
+
+// import { Container } from './styles';
+
+export const App: React.FC = () => {
+  return <Widget />;
+};
